5bbcef1ef5d3b5b192926bf5e1d1aed90e4fdf71
Craig Riecke <cr396@cornell.edu>
2016-02-11 16:56:23 -0500

    Add ICMP support

--- cross_campus_handler.py
+++ cross_campus_handler.py
@@ -15,6 +15,42 @@ class CrossCampusHandler():
     # in l2_learning_switch_handler.  
     pass
 
+  def write_table_2_rule(self, switch, dp, ip, pkt, actions, direction ):
+    parser = dp.ofproto_parser
+    if ip.proto == 0x6:
+      tcp_pkt = pkt.get_protocols(tcp.tcp)[0]
+      match = parser.OFPMatch(
+        ipv4_src=ip.src
+        ,ipv4_dst=ip.dst
+        ,eth_type=0x0800
+        ,ip_proto=ip.proto  
+        ,vlan_vid=self.nib.vlan_for_switch(switch)
+        ,tcp_src = tcp_pkt.src_port
+        ,tcp_dst = tcp_pkt.dst_port
+      )
+      src_port = tcp_pkt.src_port
+      dst_port = tcp_pkt.dst_port
+    elif ip.proto == 0x11:
+      udp_pkt = pkt.get_protocols(udp.udp)[0]
+      match = parser.OFPMatch(
+        ipv4_src=ip.src
+        ,ipv4_dst=ip.dst
+        ,eth_type=0x0800
+        ,ip_proto=ip.proto  
+        ,vlan_vid=self.nib.vlan_for_switch(switch)
+        ,udp_src = udp_pkt.src_port
+        ,udp_dst = udp_pkt.dst_port
+      )
+      src_port = udp_pkt.src_port
+      dst_port = udp_pkt.dst_port
+
+    # The flow will naturally age out after 10 minutes of idleness.  That way we can pick a new path for
+    # it if it starts up again.
+    OpenflowUtils.add_flow(dp, priority=0, match=match, actions=actions, table_id=2, idle_timeout=600)
+    self.logger.error("Added "+direction+" hash rule for "+str(ip.src)+":" + str(src_port) +  " -> "+
+      str(ip.dst) +":" + str(dst_port)
+    )
+
   def add_outgoing_dynamic_flow(self, msg):
     dp = msg.datapath
     switch = self.nib.switch_for_dp(dp)
@@ -25,13 +61,6 @@ class CrossCampusHandler():
     pkt = packet.Packet(msg.data)
     eth = pkt.get_protocols(ethernet.ethernet)[0]
     ip = pkt.get_protocols(ipv4.ipv4)[0]
-
-    # Only TCP and UDP packets can be handled by installing rules in custom pipleine hash table.  Handle
-    # all other IP protocols (like ICMP) by acting like an L2 switch.  The traffic on these protocols 
-    # should be light enough to handle all by controller.  
-    if ip.proto != 0x6 and ip.proto != 0x11:
-      return [ parser.OFPActionOutput(self.nib.router_port_for_switch(switch)) ]
-
     src_ip = ip.src
     dst_ip = ip.dst
 
@@ -65,39 +94,11 @@ class CrossCampusHandler():
     # No matter what, we always send the packet to the router
     actions.append(parser.OFPActionOutput(self.nib.router_port_for_switch(switch)))
 
-    if ip.proto == 0x6:
-      tcp_pkt = pkt.get_protocols(tcp.tcp)[0]
-      match = parser.OFPMatch(
-        ipv4_src=ip.src
-        ,ipv4_dst=ip.dst
-        ,eth_type=0x0800
-        ,ip_proto=ip.proto  
-        ,vlan_vid=self.nib.vlan_for_switch(switch)
-        ,tcp_src = tcp_pkt.src_port
-        ,tcp_dst = tcp_pkt.dst_port
-      )
-      src_port = tcp_pkt.src_port
-      dst_port = tcp_pkt.dst_port
-    elif ip.proto == 0x11:
-      udp_pkt = pkt.get_protocols(udp.udp)[0]
-      match = parser.OFPMatch(
-        ipv4_src=ip.src
-        ,ipv4_dst=ip.dst
-        ,eth_type=0x0800
-        ,ip_proto=ip.proto  
-        ,vlan_vid=self.nib.vlan_for_switch(switch)
-        ,udp_src = udp_pkt.src_port
-        ,udp_dst = udp_pkt.dst_port
-      )
-      src_port = udp_pkt.src_port
-      dst_port = udp_pkt.dst_port
-
-    # The flow will naturally age out after 10 minutes of idleness.  That way we can pick a new path for
-    # it if it starts up again.
-    OpenflowUtils.add_flow(dp, priority=0, match=match, actions=actions, table_id=2, idle_timeout=600)
-    self.logger.error("Added outgoing hash rule for "+str(ip.src)+":" + str(src_port) +  " -> "+
-      str(ip.dst) +":" + str(dst_port)
-    )
+    # Only TCP and UDP packets can be handled by installing rules in custom pipleine hash table.  Handle
+    # all other IP protocols (like ICMP) by acting like an L2 switch.  The traffic on these protocols 
+    # should be light enough to handle all by controller.  
+    if ip.proto == 0x6 or ip.proto == 0x11:
+      self.write_table_2_rule(switch, dp, ip, pkt, actions, "outgoing" )
 
     return actions
 
@@ -106,87 +107,50 @@ class CrossCampusHandler():
     switch = self.nib.switch_for_dp(dp)
     ofproto = dp.ofproto
     parser = dp.ofproto_parser
-    in_port = msg.match['in_port']
 
     pkt = packet.Packet(msg.data)
     eth = pkt.get_protocols(ethernet.ethernet)[0]
     ip = pkt.get_protocols(ipv4.ipv4)[0]
-
-    # See discussion about non-TCP and non-UDP traffic above.  Note that this side is different in that
-    # we don't necessarily know the port, so just handle like an L2 switch.
-    # TODO: We actually do have to rewrite the IP's as well, if only on the output action.      
-    if ip.proto != 0x6 and ip.proto != 0x11:
-      output_p = self.nib.port_for_mac(eth.dst)
-      if output_p == None:
-        output_p = ofproto.OFPP_FLOOD
-      return [ parser.OFPActionOutput(output_p) ]
-
     src_ip = ip.src
     dst_ip = ip.dst
 
-    # This could happen if the other side of the network is not properly rewriting the destination.
-    if NetUtils.ip_in_network(dst_ip, self.nib.actual_net_for(switch)):
-      output_p = self.nib.port_for_mac(dst)
-      if output_p == None:
-        output_p = ofproto.OFPP_FLOOD
-      return [ parser.OFPActionOutput(output_p) ]
+    # At this point, we know the destination IP is either the real network of this switch, or one
+    # virtual network.  The source may be as well.  When we're done, there should only be real
+    # networks in both the source and dest.
 
-    for ap in self.nib.alternate_paths():
-      if NetUtils.ip_in_network(dst_ip, ap["ithaca"]): 
-        switch = "ithaca" 
-        imaginary_net = ap["ithaca"]
-      elif NetUtils.ip_in_network(dst_ip, ap["nyc"]):
-        switch = "nyc"
-        imaginary_net = ap["nyc"]
-    real_net = self.nib.actual_net_for(switch)
-    dst_host = NetUtils.host_of_ip(dst_ip, imaginary_net)
-    new_dest_ip = NetUtils.ip_for_network(real_net, dst_host)
+    # If the destination IP is in the real network (ip_in_network returns true), 
+    # then the other side of the network is not properly rewriting the destination.
+    # or the packet is coming from the non-Coscin Internet.  Just leave the IP's alone (we assume 
+    # there's nothing crazy like the source using a virtual address here.)
+    actions = []
+    if not NetUtils.ip_in_network(dst_ip, self.nib.actual_net_for(switch)):
+      # TODO: Move this logic to NIB()
+      for ap in self.nib.alternate_paths():
+        if NetUtils.ip_in_network(dst_ip, ap["ithaca"]): 
+          switch = "ithaca" 
+          imaginary_net = ap["ithaca"]
+        elif NetUtils.ip_in_network(dst_ip, ap["nyc"]):
+          switch = "nyc"
+          imaginary_net = ap["nyc"]
+      real_net = self.nib.actual_net_for(switch)
+      dst_host = NetUtils.host_of_ip(dst_ip, imaginary_net)
+      new_dest_ip = NetUtils.ip_for_network(real_net, dst_host)
+      new_src_ip = self.nib.translate_alternate_net(src_ip)    
+      actions.append( parser.OFPActionSetField(ipv4_src=new_src_ip) )
+      actions.append( parser.OFPActionSetField(ipv4_dst=new_dest_ip) )
 
-    # If the ip hasn't yet been learned yet, just flood it out to all ports without installing a rule.  The 
-    # recipient should naturally reply, causing learning to occur, and the next packet will install the rule.  
-    if not self.nib.learned_ip(new_dest_ip):
-      return [ parser.OFPActionOutput(ofproto.OFPP_FLOOD) ]
+    # The destination mac in the packet is guaranteed OK because the router placed it there as a result
+    # of its ARP cache.  However, that doesn't necessarily mean we have learned that port yet, so act like
+    # an L2 switch.  But in that case, don't install the rule because we don't want to just flood the switch
+    # everytime it happens.  
+    output_p = self.nib.port_for_mac(eth.dst)
+    if output_p == None:
+      output_p = ofproto.OFPP_FLOOD
 
-    direct_net_port = self.nib.port_for_ip(new_dest_ip)
-    new_src_ip = self.nib.translate_alternate_net(src_ip)    
+    actions.append(parser.OFPActionOutput(output_p))
 
-    if ip.proto == 0x6:
-      tcp_pkt = pkt.get_protocols(tcp.tcp)[0]
-      match = parser.OFPMatch(
-        ipv4_src=ip.src
-        ,ipv4_dst=ip.dst
-        ,eth_type=0x0800
-        ,ip_proto=ip.proto  
-        ,vlan_vid=self.nib.vlan_for_switch(switch)
-        ,tcp_src = tcp_pkt.src_port
-        ,tcp_dst = tcp_pkt.dst_port
-      )
-      src_port = tcp_pkt.src_port
-      dst_port = tcp_pkt.dst_port
-    elif ip.proto == 0x11:
-      udp_pkt = pkt.get_protocols(udp.udp)[0]
-      match = parser.OFPMatch(
-        ipv4_src=ip.src
-        ,ipv4_dst=ip.dst
-        ,eth_type=0x0800
-        ,ip_proto=ip.proto  
-        ,vlan_vid=self.nib.vlan_for_switch(switch)
-        ,udp_src = udp_pkt.src_port
-        ,udp_dst = udp_pkt.dst_port
-      )
-      src_port = udp_pkt.src_port
-      dst_port = udp_pkt.dst_port
-
-    actions = [
-      parser.OFPActionSetField(ipv4_src=new_src_ip),
-      parser.OFPActionSetField(ipv4_dst=new_dest_ip),
-      parser.OFPActionOutput(direct_net_port)
-    ]
-    # The flow will naturally age out after 10 minutes of idleness.  That keeps the table clean.
-    OpenflowUtils.add_flow(dp, priority=0, match=match, actions=actions, table_id=2, idle_timeout=600)
-    self.logger.error("Added incoming hash rule for "+str(ip.src)+":" + str(src_port) +  " -> "+
-      str(ip.dst) +":" + str(dst_port)
-    )
+    if (ip.proto == 0x6 or ip.proto == 0x11) and output_p != ofproto.OFPP_FLOOD:
+      self.write_table_2_rule(switch, dp, ip, pkt, actions, "incoming" )
 
     return actions
 
