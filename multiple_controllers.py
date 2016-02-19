8cea791d42165ba7afc23945d64b8677bc2def45
Craig Riecke <cr396@cornell.edu>
2016-02-19 11:05:08 -0500

    Use locks instead of elections in Zookeeper

--- multiple_controllers.py
+++ multiple_controllers.py
@@ -2,8 +2,16 @@
 import kazoo.client
 from ryu.ofproto import ofproto_v1_3
 
-ELECTION_PATH = "/multiple_controllers/master"
+LOCK_PATH = "/multiple_controllers/master"
 DEFAULT_TIMEOUT = 10  
+
+def my_listener(state):
+  if state == KazooState.LOST:
+    self.logger.info("Zookeeper session lost")
+  elif state == KazooState.SUSPENDED:
+    self.logger.info("Zookeeper session suspended")
+  else:
+    self.logger.info("Zookeeper session connected/reconnected")
 
 class MultipleControllers(object):
   OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -14,15 +22,12 @@ class MultipleControllers(object):
     self.logger = logger
     self.zk = kazoo.client.KazooClient(hosts = zhost, timeout = DEFAULT_TIMEOUT)
     self.zk.start()
+    self.zk.add_listener(my_listener)
 
   def role_request(self, dp, role):
     ofp_parser = dp.ofproto_parser
     msg = ofp_parser.OFPRoleRequest(dp, role, 0)
     dp.send_msg(msg)
-
-  def promote_to_primary(self):
-    self.logger.info("Promoted to master controller")
-    self.role_request(self.dp, ofproto_v1_3.OFPCR_ROLE_MASTER)
 
   def handle_datapath(self, ev):
     # We always start as a slave.  If we win the election, we bump ourselves up to master 
@@ -30,7 +35,10 @@ class MultipleControllers(object):
     self.role_request(self.dp, ofproto_v1_3.OFPCR_ROLE_SLAVE)
     self.logger.info("Beginning in backup controller state.  If there's a working primary controller, we'll wait here.")
 
-    self.election = self.zk.Election(ELECTION_PATH, self.host)
+    self.lock = self.zk.Lock(LOCK_PATH, self.host)
 
-    # The following will block until this controller wins the election
-    self.election.run(self.promote_to_primary)
+    # The following will block until this controller wins the lock
+    self.lock.acquire()
+
+    self.logger.info("Promoted to master controller")
+    self.role_request(self.dp, ofproto_v1_3.OFPCR_ROLE_MASTER)
